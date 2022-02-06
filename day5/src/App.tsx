b88bed6f09eaa7e79779e4722777589a74bb33d2
Jeremi <Kelhen@users.noreply.github.com>
2022-02-06 20:40:59 +0900

    [day5] setup prettier and format code

--- day5/src/App.tsx
+++ day5/src/App.tsx
@@ -1,14 +1,13 @@
-import { useState } from 'react';
-import Calculator from './components/Calculator';
+import Calculator from "./components/Calculator";
 
 function App() {
   return (
-    <div className="h-screen bg-purple-600 text-white flex items-center justify-center">
-      <div className='h-full w-full sm:h-auto sm:w-3/4 p-4 sm:rounded'>
+    <div className="flex h-screen items-center justify-center bg-purple-600 text-white">
+      <div className="h-full w-full p-4 sm:h-auto sm:w-3/4 sm:rounded">
         <Calculator />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- day5/src/components/Calculator.tsx
+++ day5/src/components/Calculator.tsx
@@ -6,28 +6,64 @@ interface FormInput {
 }
 
 export default function SuperSecret() {
-  const [total, setTotal] = React.useState<null|number>(0);
-  const [formInputs, setFormInputs] = React.useState<FormInput>({ number1: null, number2: null });
+  const [total, setTotal] = React.useState<null | number>(0);
+  const [formInputs, setFormInputs] = React.useState<FormInput>({
+    number1: null,
+    number2: null,
+  });
 
   function calculateTotal() {
-    setTotal(parseFloat(formInputs.number1 || 0) + parseFloat(formInputs.number2 || 0));
+    setTotal(
+      parseFloat(formInputs.number1 || 0) + parseFloat(formInputs.number2 || 0)
+    );
   }
 
   return (
-    <div className="space-y-4 mx-auto">
-      <h2 className='text-center text-2xl'>Adding two Numbers</h2>
-      <form onSubmit={(e) => { e.preventDefault(); calculateTotal() }} className="text-gray-900 grid gap-4">
-        <div className="grid grid-rows sm:grid-cols-2 gap-4 text-4xl">
-          <input type="number" step="0.1" className="px-1 py-2 text-center text-purple-500 bg-purple-200 rounded" onChange={(e) => setFormInputs({ ...formInputs, number1: parseFloat(e.target.value) })} />
-          <input type="number" step="0.1" className="px-1 py-2 text-center text-purple-500 bg-purple-200 rounded" onChange={(e) => setFormInputs({ ...formInputs, number2: parseFloat(e.target.value) })} />
+    <div className="mx-auto space-y-4">
+      <h2 className="text-center text-2xl">Adding two Numbers</h2>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          calculateTotal();
+        }}
+        className="grid gap-4 text-gray-900"
+      >
+        <div className="grid-rows grid gap-4 text-4xl sm:grid-cols-2 ">
+          <input
+            type="number"
+            step="0.1"
+            className="rounded bg-purple-200 px-1 py-2 text-center text-purple-500"
+            onChange={(e) =>
+              setFormInputs({
+                ...formInputs,
+                number1: parseFloat(e.target.value),
+              })
+            }
+          />
+          <input
+            type="number"
+            step="0.1"
+            className="rounded bg-purple-200 px-1 py-2 text-center text-purple-500"
+            onChange={(e) =>
+              setFormInputs({
+                ...formInputs,
+                number2: parseFloat(e.target.value),
+              })
+            }
+          />
         </div>
         <div className="mx-auto">
-          <button className="p-4 bg-purple-800 text-purple-200 rounded-tl-lg rounded-br-lg rounded-tr-2xl rounded-bl-2xl" type="submit">Add Them!</button>
+          <button
+            className="rounded-tl-lg rounded-br-lg rounded-tr-2xl rounded-bl-2xl bg-purple-800 p-4 text-purple-200"
+            type="submit"
+          >
+            Add Them!
+          </button>
         </div>
       </form>
-      <div className="text-center font-bold text-5xl">
+      <div className="text-center text-5xl font-bold">
         <p>{total}</p>
       </div>
-    </div >
-  )
+    </div>
+  );
 }
