6968076d4788a68aa14a1f7f8f86df2d0a769722
Jeremi <Kelhen@users.noreply.github.com>
2022-02-06 18:37:53 +0900

    [day5] improve type and code clarity

--- day5/src/components/Calculator.tsx
+++ day5/src/components/Calculator.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 
-export default function SuperSecret() {
-  const [count, setCount] = React.useState<null|number>(0);
+interface FormInput {
+  number1: number | null;
+  number2: number | null;
+}
 
-  const [formInputs, setFormInputs] = React.useState<{ number1: number | null, number2: number | null }>({ number1: null, number2: null });
+export default function SuperSecret() {
+  const [total, setTotal] = React.useState<null|number>(0);
+  const [formInputs, setFormInputs] = React.useState<FormInput>({ number1: null, number2: null });
+
+  function calculateTotal() {
+    setTotal(parseFloat(formInputs.number1 || 0) + parseFloat(formInputs.number2 || 0));
+  }
 
   return (
     <div className="space-y-4 mx-auto">
       <h2 className='text-center text-2xl'>Adding two Numbers</h2>
-      <form onSubmit={(e) => { e.preventDefault(); setCount(parseFloat(formInputs.number1 || 0) + parseFloat(formInputs.number2 || 0)) }} className="text-gray-900 grid gap-4">
+      <form onSubmit={(e) => { e.preventDefault(); calculateTotal() }} className="text-gray-900 grid gap-4">
         <div className="grid grid-rows sm:grid-cols-2 gap-4 text-4xl">
           <input type="number" step="0.1" className="px-1 py-2 text-center text-purple-500 bg-purple-200 rounded" onChange={(e) => setFormInputs({ ...formInputs, number1: parseFloat(e.target.value) })} />
           <input type="number" step="0.1" className="px-1 py-2 text-center text-purple-500 bg-purple-200 rounded" onChange={(e) => setFormInputs({ ...formInputs, number2: parseFloat(e.target.value) })} />
@@ -18,7 +26,7 @@ export default function SuperSecret() {
         </div>
       </form>
       <div className="text-center font-bold text-5xl">
-        <p>{count}</p>
+        <p>{total}</p>
       </div>
     </div >
   )
